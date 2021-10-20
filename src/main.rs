72ebed03d10ebcd26a25e0afb95d0184dd70c3c8
CowCowFuture <82391199+CowCowFuture@users.noreply.github.com>
2021-10-19 20:44:10 -0500

    Added position and walls

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
+use num::clamp;
+
 fn main() {
     let mut game_loop = true;
+    let mut posx: i8 = 0;
+    let mut posy: i8 = 0;
 
     while game_loop {
         let mut input = String::new();
@@ -8,6 +12,17 @@ fn main() {
         if input == "q" {
             game_loop = false;
         }
+        if input == "w" { posy+=1; }
+        if input == "a" { posx-=1; }
+        if input == "s" { posy-=1; }
+        if input == "d" { posx+=1; }
+        let tempx = posx;
+        let tempy = posy;
+        posx = clamp(posx, -8, 8);
+        posy = clamp(posy, -8, 8);
+        if tempx != posx { println!("You've bumped into a wall!"); }
+        if tempy != posy { println!("You've bumped into a wall!"); }
+        println!("{}, {}", posx, posy);
 
     }
 }

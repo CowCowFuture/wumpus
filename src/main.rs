e2319642b945c14e8edefaab3787c1dd2548ae29
CowCowFuture <82391199+CowCowFuture@users.noreply.github.com>
2021-10-19 20:18:50 -0500

    Added game loop

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
 fn main() {
-    println!("Hello, world!");
+    let mut game_loop = true;
+
+    while game_loop {
+        let mut input = String::new();
+        input = spec_read(input);
+
+        if input == "q" {
+            game_loop = false;
+        }
+
+    }
+}
+
+fn spec_read(mut string: String) -> String {
+    std::io::stdin().read_line(&mut string).expect("borked");
+    string.pop();
+    string = string.to_lowercase();
+
+    return string;
 }
